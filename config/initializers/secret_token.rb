1a90aaf27203a352b2bbaa25c4116b2a3c672ff7
ericminio <eric.mignot@gmail.com>
2012-03-08 18:03:33 -0500

    logo alone on black page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Symphonie::Application.config.secret_token = '209251acd2000ae1ddd6971c96d38687837c432ec90e49f439f89236fc47f7803889f157964f4a601f03dfafe192f9db890d440ca6d2bd28320ae1878c495965'
